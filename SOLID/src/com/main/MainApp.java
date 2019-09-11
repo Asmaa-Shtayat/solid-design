3d8235bd27b7bf5bbe8639d90656b0b5e804b90f
ali.alkhatib <ali-5@DESKTOP-ITTRLEU>
2019-09-11 06:55:33 +0300

    Adding new project files

---
+++ SOLID/src/com/main/MainApp.java
@@ -0,0 +1,15 @@
+package com.main;
+
+/**
+ * 
+ * @author ali.alkhatib
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
